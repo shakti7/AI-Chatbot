6d32460a8bb442e59068e0eb2fccec77f5d45752
shakti7 <shaktiprasanna.sahoo.7@gmail.com>
2025-08-14 02:55:00 +0530

    fix: correct import statements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import ThemeToggle from './components/ThemeToggle'
 import TypingLoader from './components/TypingLoader'
 import SidebarSessions from './components/SidebarSessions'
 import MessageMarkdown from './components/MessageMarkdown'
-import { connectSSE } from './lib/sse'
+import { connectSSE } from './lib/sse.js'
 import {
   newSession,
   selectSession,
